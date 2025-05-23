bf352b7f9e47ac5a9478256bf82d1a4473567ee3
Nick Klauer <klauer@gmail.com>
2025-05-22 22:38:05 -0500

    test: fix them

--- test/controllers/dashboard_controller_test.rb
+++ test/controllers/dashboard_controller_test.rb
@@ -2,6 +2,11 @@ require "test_helper"
 
 class DashboardControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    # Sign in as the first test user
+    post session_url, params: { email_address: users(:one).email_address, password: "password" }
+    assert_redirected_to root_url
+
+    # Now try to access the dashboard
     get dashboard_index_url
     assert_response :success
   end

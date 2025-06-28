d470e99608fe546773d1a6f2055a8991b6ad0bf5
Nick Klauer <klauer@gmail.com>
2025-06-28 14:10:29 -0500

    chore: rubocop fixes

--- lib/cli/prpal_cli.rb
+++ lib/cli/prpal_cli.rb
@@ -1,15 +1,15 @@
-require 'thor'
+require "thor"
 
 module Cli
   class Reviews < Thor
-    desc 'list', 'List all pull request reviews'
+    desc "list", "List all pull request reviews"
     def list
       PullRequestReview.find_each do |review|
         puts "#{review.id}: #{review.github_pr_title} [#{review.status}]"
       end
     end
 
-    desc 'show ID', 'Show details for a pull request review'
+    desc "show ID", "Show details for a pull request review"
     def show(id)
       review = PullRequestReview.find(id)
       puts "ID: #{review.id}"
@@ -22,8 +22,7 @@ end
 
 module Cli
   class PrpalCli < Thor
-    desc 'reviews SUBCOMMAND ...ARGS', 'Manage pull request reviews'
-    subcommand 'reviews', Reviews
+    desc "reviews SUBCOMMAND ...ARGS", "Manage pull request reviews"
+    subcommand "reviews", Reviews
   end
 end
-
--- test/cli/prpal_cli_test.rb
+++ test/cli/prpal_cli_test.rb
@@ -1,20 +1,20 @@
-require 'test_helper'
-require 'cli/prpal_cli'
+require "test_helper"
+require "cli/prpal_cli"
 
 class PrpalCliTest < ActiveSupport::TestCase
-  test 'list command outputs reviews' do
+  test "list command outputs reviews" do
     output, = capture_io do
-      Cli::PrpalCli.start(['reviews', 'list'])
+      Cli::PrpalCli.start([ "reviews", "list" ])
     end
 
     assert_includes output, pull_request_reviews(:review_pr_one).github_pr_title
     assert_includes output, pull_request_reviews(:review_pr_two).github_pr_title
   end
 
-  test 'show command outputs details' do
+  test "show command outputs details" do
     review = pull_request_reviews(:review_pr_one)
     output, = capture_io do
-      Cli::PrpalCli.start(['reviews', 'show', review.id.to_s])
+      Cli::PrpalCli.start([ "reviews", "show", review.id.to_s ])
     end
 
     assert_includes output, review.github_pr_title
